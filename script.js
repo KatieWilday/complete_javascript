321facfd14001a8592eba1258f098dd6ad8660cb
Katie Wilday <wildayk@gmail.com>
2020-05-05 20:48:08 -0400

    challenge 5

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ while(i < john.length){
 */
 
 //Contine and break statements - to break out of loop
-john = ["john", "smith", 1990, "designer", "false"]
+/*john = ["john", "smith", 1990, "designer", "false"]
 
 for (i = 0; i < john.length; i++) {
   if (typeof john[i] !== 'string') continue //continue to next
@@ -290,4 +290,77 @@ for (i = 0; i < john.length; i++) {
 //Looping backward
 for (i = john.length - 1; i >= 0; i--){
   console.log(john[i])
+}
+*/
+//Coding challenge 5
+john = {
+  fullName: "John Smith",
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function() {
+    this.tips = []
+    this.finalValues = []
+
+    for (i = 0; i < this.bills.length; i++) {
+      var percentage
+      bill = this.bills[i] //created so we don't have to repeat
+      if (bill < 50) {
+          percentage = .2;
+        } else if (bill >= 50 && bill < 200) {
+          percentage = .15;
+        } else {
+          percentage = .1;
+        }
+
+        this.tips[i] = bill * percentage;
+        this.finalValues[i] = bill + bill * percentage;
+      }
+    }
+}
+
+john.calcTips()
+console.log(john)
+
+mark = {
+  fullName: "Mark Smith",
+  bills: [77, 375, 110, 45],
+  calcTips: function() {
+    this.tips = []
+    this.finalValues = []
+
+    for (i = 0; i < this.bills.length; i++) {
+      var percentage
+      bill = this.bills[i]
+      if (bill < 100) {
+          percentage = .2;
+        } else if (bill >= 100 && bill < 300) {
+          percentage = .1;
+        } else {
+          percentage = .25;
+        }
+
+        this.tips[i] = bill * percentage;
+        this.finalValues[i] = bill + bill * percentage;
+      }
+    }
+}
+
+function calcAverage(tips) {
+    sum = 0;
+    for (i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
+john.calcTips()
+mark.calcTips()
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
 }
