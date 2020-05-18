b7f9f2de90a21ec4bd0d9e41c8eb883200d6ebc3
Katie Wilday <wildayk@gmail.com>
2020-05-18 11:06:41 -0400

    getelementbyid

--- Dom-Pig-Game/app.js
+++ Dom-Pig-Game/app.js
@@ -13,8 +13,25 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1
-console.log(dice)
 
-//document.querySelector("#current-" + activePlayer).textContent = dice;
-document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>"
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+
+  //1. Random number
+  dice = Math.floor(Math.random() * 6) + 1
+
+  //2. Display result
+  diceDOM = document.querySelector(".dice")
+  diceDOM.style.display = "block"
+  diceDOM.src = "dice-" + dice + ".png"
+
+  //3. Update round score IF the rolled number was NOT 1
+
+});
