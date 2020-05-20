8f8a19a21aa81dccbe2e9ec54df5cf63cd230184
Katie Wilday <wildayk@gmail.com>
2020-05-20 06:47:24 -0400

    Function Constructors

--- Sections/section5/script.js
+++ Sections/section5/script.js
@@ -10,10 +10,13 @@ Person = function(name, yearOfBirth, job) {
   this.name = name
   this.yearOfBirth = yearOfBirth
   this.job = job
-  this.calcAge = function() {
-    console.log(2020 - this.yearOfBirth)
-  }
 }
+
+Person.prototype.calcAge = function() {
+  console.log(2020 - this.yearOfBirth)
+}
+
+Person.prototype.lastName = "Smith"
 
 john = new Person("John", 1990, "teacher") //new empty object is corrected, then Person function is called.
 katie = new Person("Katie", 1988, "graphic designer")
@@ -22,3 +25,5 @@ jane = new Person("Jane", 1950, "bus driver")
 john.calcAge()
 katie.calcAge()
 jane.calcAge()
+
+console.log(john.lastName, jane.lastName, katie.lastName)
