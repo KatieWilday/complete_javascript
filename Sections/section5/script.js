3d4550fa590644e8f1811b87789d149a0cf9996b
Katie Wilday <wildayk@gmail.com>
2020-05-20 06:43:47 -0400

    function constructor

---
+++ Sections/section5/script.js
@@ -0,0 +1,24 @@
+//Function Constructor: pattern for a "blueprint"
+
+john = {
+  name: "John",
+  yearOfBirth: 1990,
+  job: "teacher"
+}
+
+Person = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+  this.calcAge = function() {
+    console.log(2020 - this.yearOfBirth)
+  }
+}
+
+john = new Person("John", 1990, "teacher") //new empty object is corrected, then Person function is called.
+katie = new Person("Katie", 1988, "graphic designer")
+jane = new Person("Jane", 1950, "bus driver")
+
+john.calcAge()
+katie.calcAge()
+jane.calcAge()
