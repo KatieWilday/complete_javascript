09a14ebc05f5ac20afd8dd2d404c3fd28d3cbdbe
Katie Wilday <wildayk@gmail.com>
2020-05-21 07:02:06 -0400

    ui controller

--- Sections/section6/app.js
+++ Sections/section6/app.js
@@ -8,20 +8,32 @@ budgetController = (function(){
 //User Interface Controller
 uIController = (function() {
 
-    //Some code
+  return { //available globally
+    getInput: function(){
+      return {
+        type: document.querySelector(".add__type").value, //will be either inc or exp
+        description: document.querySelector(".add__description").value,
+        value: document.querySelector(".add__value").value
+      }
+    }
+  }
 
 })();
 
 //Global App Controller
 controller = (function(budgetCtrl, uICtrl){
+
   function ctrlAddItem() {
     //1. Get field input data
+    input = uICtrl.getInput()
+    console.log(input)
+
     //2. Add item to budget Controller
     //3. Add new item to Interface
     //4. Calculate budget
     //5. Display budget
-    console.log("katie")
   }
+
   document.querySelector(".add__btn").addEventListener("click", ctrlAddItem)
 
   document.addEventListener("keypress", function(e){
