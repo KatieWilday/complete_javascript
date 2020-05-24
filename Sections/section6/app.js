2a0872d084667ac3400a3e7b8509bb0956562df6
Katie Wilday <wildayk@gmail.com>
2020-05-24 09:55:50 -0400

    calculate budget

--- Sections/section6/app.js
+++ Sections/section6/app.js
@@ -13,6 +13,14 @@ budgetController = (function(){
     this.value = value
   }
 
+  calcTotal = function(type) {
+    sum = 0
+    data.allItems[type].forEach(function(cur){
+      sum += cur.value
+    })
+    data.totals[type] = sum
+  }
+
   data = {
     allItems: {
       exp: [],
@@ -21,7 +29,9 @@ budgetController = (function(){
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   }
 
   return {
@@ -48,6 +58,33 @@ budgetController = (function(){
       //Return the new element
       return newItem
     },
+
+    calculateBudget: function() {
+        // calculate total income and expenses
+        calcTotal("exp")
+        calcTotal("inc")
+
+        //calculate budget" income - expenses
+        data.budget = data.totals.inc - data.totals.exp
+
+        //calculate percentage of income that we spent
+        if(data.totals.inc > 0) {
+          data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
+        } else {
+          data.percentage = -1
+        }
+
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
+    },
+
     testing: function(){
       console.log(data)
     }
@@ -136,9 +173,11 @@ controller = (function(budgetCtrl, uICtrl){
   }
   function updateBudget() {
     //1. Calculate budget
+    budgetCtrl.calculateBudget();
 
     //2. Returns the budget
-
+    budget = budgetCtrl.getBudget();
+    console.log(budget)
     //3. Display budget
   }
 
