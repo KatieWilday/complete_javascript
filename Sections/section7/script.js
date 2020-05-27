e13eab07564195c453fb7bc2c445442c7ac79874
Katie Wilday <wildayk@gmail.com>
2020-05-27 08:58:39 -0400

    let and const

---
+++ Sections/section7/script.js
@@ -0,0 +1,17 @@
+//let and const
+//let can be changed, const cannot
+//block scoped
+
+/*
+const n = 5
+
+for(let i = 0; i < n; i++) {
+  let btn = document.createElement("button")
+  btn.appendChild(document.createTextNode("Button " + i))
+  btn.addEventListener("click", function(){
+    console.log(i)
+  })
+
+  document.body.appendChild(btn)
+}
+*/
