dcd25e5b2a6b365a99fc6f3ee1c4afb6d8a22aba
Katie Wilday <wildayk@gmail.com>
2020-05-28 10:55:20 -0400

    maps

--- Sections/section7/script.js
+++ Sections/section7/script.js
@@ -268,7 +268,7 @@ emily = new SmithPerson("Emily", 2011, "Diaz", "Spanish")
 */
 
 //ES6
-
+/*
 function SmithPerson(firstName, yearOfBirth, lastName = "Smith", nationality = "American") {
   this.firstName = firstName
   this.yearOfBirth = yearOfBirth
@@ -279,3 +279,36 @@ function SmithPerson(firstName, yearOfBirth, lastName = "Smith", nationality = "
 
 john = new SmithPerson("John", 1990)
 emily = new SmithPerson("Emily", 2011, "Diaz", "Spanish")
+*/
+
+//Maps
+//map string keys to arbitrary values - can use any value booleans, strings, etc.
+
+const question = new Map()
+question.set("question", "What is the offical name of the latest major JavaScript version?")
+question.set(1, "ES5")
+question.set(2, "ES6")
+question.set(3, "ES2015")
+question.set(4, "ES7")
+question.set("correct", 3)
+question.set(true, "Correct!")
+question.set(false, "Try Again!")
+
+console.log(question.get("question"))
+//console.log(question.size)
+
+if(question.has(4)) {
+  //question.delete(4)
+  //console.log("answer 4 is here")
+}
+
+//question.forEach((value, key) => console.log(`This is ${key} and it's ${value}`))
+
+for(let [key, value] of question.entries()) {
+  if (typeof(key) === "number") {
+    console.log(`Answer ${key}: ${value}`)
+  }
+}
+
+const ans = parseInt(prompt("Write the correct answer"))
+console.log(question.get(ans === question.get("correct")))
