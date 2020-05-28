e4a5bc485cd46617f1b5b4795c41a51cf0d6a50f
Katie Wilday <wildayk@gmail.com>
2020-05-28 11:25:26 -0400

    sub classes

--- Sections/section7/script.js
+++ Sections/section7/script.js
@@ -318,6 +318,7 @@ console.log(question.get(ans === question.get("correct")))
 //make it easier to impliment inheritance
 
 //ES5
+/*
 Person5 = function(name, yearOfBirth, job) {
   this.name = name
   this.yearOfBirth = yearOfBirth
@@ -333,6 +334,7 @@ john5 = new Person5("John", 1990, "teacher")
 
 
 //ES6
+
 class Person6 {
   constructor (name, yearOfBirth, job) {
     this.name = name
@@ -353,3 +355,63 @@ class Person6 {
 const katie6 = new Person6("Katie", 1988, "software engineer")
 
 Person6.greeting()
+
+*/
+
+//Classes with Sub Classes
+//ES5
+/*
+Person5 = function(name, yearOfBirth, job) { //super class
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person5.prototype.calcAge = function() {
+  age = new Date().getFullYear() - this.yearOfBirth
+  console.log(age)
+}
+
+Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) { //sub class
+  Person5.call(this, name, yearOfBirth, job)
+  this.olympicGames = olympicGames
+  this.medals = medals
+}
+
+Athlete5.prototype = Object.create(Person5.prototype)
+
+johnAthlete5 = new Athlete5("John", 1990, "swimmer", 4, 10)
+
+johnAthlete5.calcAge()
+*/
+
+//ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calcAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth
+    console.log(age)
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job)
+    this.olympicGames = olympicGames
+    this.medals = medals
+  }
+
+  wonMedal(){
+    this.medals++
+    console.log(this.medals)
+  }
+}
+
+  const johnAthlete6 = new Athlete6("John", 1990, "swimmer", 3, 10)
+  johnAthlete6.wonMedal()
+  johnAthlete6.calcAge()
