55135c30338b2623c9973cc4d1baf4e4a8b532b7
Katie Wilday <wildayk@gmail.com>
2020-05-28 20:03:11 -0400

    promises

--- Sections/section8/script.js
+++ Sections/section8/script.js
@@ -36,3 +36,44 @@ getRecipe()
 */
 
 //Promises
+//Object that keeps track about whether a certain event has happened or not
+
+const getIDs = new Promise((resolve, reject) => { //executer function takes resolve, reject
+  setTimeout(() => { //Asynchronous code
+    resolve([15, 12, 26, 64]) //successful-pass in data
+  }, 1500)
+})
+
+const getRecipe = recID => {
+  return new Promise((resolve, reject) => {
+    setTimeout(id => {
+      const recipe = {title: "Fresh Tomato Pasta", publisher: "Katie"}
+      resolve(`${id}: ${recipe.title}`)
+    }, 1500, recID)
+  })
+}
+
+const getRelated = publisher => {
+  return new Promise((resolve, reject) => {
+    setTimeout(pub => {
+      const recipe = {title: "Italian Pizza", publisher: "Katie"}
+      resolve(`${pub}: ${recipe.title}`)
+    }, 1500, publisher)
+  })
+}
+
+getIDs
+.then(ids => { //used to handle fulfilled promise
+  console.log(ids) //result of succesful promise
+  return getRecipe(ids[2])
+})
+.then(recipe => {
+  console.log(recipe)
+  return getRelated("Katie")
+})
+.then(recipe => {
+  console.log(recipe)
+})
+.catch(error => {
+  console.log(error)
+})
