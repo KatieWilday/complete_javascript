b5cdc7a32dbf1d5ee08f2b47d475c1938b76e9fd
Katie Wilday <wildayk@gmail.com>
2020-06-04 20:03:37 -0400

    bonus

---
+++ Bonus/index.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/starter/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+
+  console.log(url.parse(req.url, true));
+
+  //PRODUCT OVERVIEW
+  if(pathName === '/products' || pathName === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+
+    fs.readFile(`${__dirname}/starter/templates/template-overview.html`, `utf-8`, (err, data) => {
+      let overviewOutput = data;
+
+      fs.readFile(`${__dirname}/starter/templates/template-card.html`, `utf-8`, (err, data) => {
+        const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+        overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+
+        res.end(overviewOutput);
+      });
+    });
+
+
+  //LAPTOP DETAIL
+  } else if (pathName === '/laptop' && id < laptopData.length) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+
+    fs.readFile(`${__dirname}/starter/templates/template-laptop.html`, `utf-8`, (err, data) => {
+      const laptop = laptopData[id];
+      const output = replaceTemplate(data, laptop);
+      res.end(output);
+    });
+
+  }
+
+  //URL NOT FOUND
+  else {
+    res.writeHead(404, { 'Content-type': 'text/html'});
+    res.end('URL was not found on server');
+  }
+
+});
+
+server.listen(1337, '127.0.0.1', () => {
+  console.log("listening for requests")
+})
+
+
+function replaceTemplate(originalHtml, laptop) {
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price)
+  output = output.replace(/{%SCREEN%}/g, laptop.screen)
+  output = output.replace(/{%CPU%}/g, laptop.cpu)
+  output = output.replace(/{%STORAGE%}/g, laptop.storage)
+  output = output.replace(/{%RAM%}/g, laptop.RAM)
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
+  output = output.replace(/{%ID%}/g, laptop.id)
+  return output;
+}
